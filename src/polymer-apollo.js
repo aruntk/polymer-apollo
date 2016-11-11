a9461103c31e9832c36b1d4a2cd12a8123d7a1fa
aruntk <aruntknambiar@gmail.com>
2016-11-11 21:05:02 +0530

    linting

--- src/polymer-apollo.js
+++ src/polymer-apollo.js
@@ -1,66 +1,21 @@
+/* eslint-disable no-param-reassign, func-names, no-console */
 import omit from 'lodash.omit';
-import map from 'lodash.map';
-function deepFind(obj, path) {
-  let paths = path.split('.'),
-    current = obj,
-    i;
 
-  for (i = 0; i < paths.length; ++i) {
+function deepFind(obj, path) {
+  const paths = path.split('.');
+  let current = obj;
+  let i;
+
+  for (i = 0; i < paths.length; i += 1) {
     if (current[paths[i]] === undefined) {
       return undefined;
-    } else {
-      current = current[paths[i]];
     }
+    current = current[paths[i]];
   }
   return current;
 }
-export class PolymerApollo {
-  constructor(options){
-    this._apolloClient = options.apolloClient;
-    this._apolloSubscriptions = [];
-    this.properties = {};
-  }
-  beforeRegister(){
-    let apollo = this.apollo;
-    this.$apollo = new DollarApollo(this);
 
-    if (apollo) {
-      const queries = omit(apollo, [
-        'subscribe',
-      ]);
-
-      // watchQuery
-      for (let key in queries) {
-        this.$apollo.process(key, queries[key]);
-
-      }
-
-      // subscribe
-      if (apollo.subscribe) {
-        // TODO
-      }
-    }
-
-  }
-
-  attached(){
-    let apollo = this.apollo;
-
-    this.$apollo.processObservers(this);
-  }
-  detached(){
-    this._apolloSubscriptions.forEach((sub) => {
-      sub.unsubscribe();
-    });
-    this._apolloSubscriptions = null;
-    if (this.$apollo) {
-      this.$apollo = null;
-    }
-
-  }
-}
-
-class DollarApollo {
+export class DollarApollo {
   constructor(el) {
     this.el = el;
     this.querySubscriptions = {};
@@ -79,20 +34,18 @@ class DollarApollo {
     const el = this.el;
     const observable = this.client.watchQuery(options);
     const _subscribe = observable.subscribe.bind(observable);
-    observable.subscribe = (function(options) {
-      let sub = _subscribe(options);
+    observable.subscribe = (function (opt) {
+      const sub = _subscribe(opt);
 
       const _unsubscribe = sub.unsubscribe.bind(sub);
-      sub.unsubscribe = function() {
+      sub.unsubscribe = function () {
         _unsubscribe();
-        el._apolloSubscriptions = el._apolloSubscriptions.filter((storeSub) => {
-          return storeSub !== sub;
-        });
-      }.bind(sub);
+        el._apolloSubscriptions = el._apolloSubscriptions.filter(storeSub => storeSub !== sub);
+      };
 
       el._apolloSubscriptions.push(sub);
       return sub;
-    }).bind(observable);
+    });
 
     return observable;
   }
@@ -100,40 +53,39 @@ class DollarApollo {
   get mutate() {
     return this.client.mutate;
   }
-  processObservers (el) {
+
+  processObservers(el) {
     // Create subscription
     const $apollo = this;
     this.el = el;
-    for(let i in el.__apollo_store){
+    for (const i of Object.keys(el.__apollo_store)) {
       const obj = el.__apollo_store[i];
       if (obj.options.skip === undefined) {
-        $apollo._subscribeObservers(i,obj.options,obj.observer);
+        $apollo._subscribeObservers(i, obj.options, obj.observer);
       }
     }
   }
-  _subscribeObservers(key,options,observer){
+
+  _subscribeObservers(key, options, observer) {
     const el = this.el;
     const $apollo = this;
-    let loadingKey = options.loadingKey;
+    const loadingKey = options.loadingKey;
     let loadingChangeCb = options.watchLoading;
-    this._changeLoader(loadingKey,true,loadingChangeCb);
+    this._changeLoader(loadingKey, true, loadingChangeCb);
     if (typeof loadingChangeCb === 'function') {
       loadingChangeCb = loadingChangeCb.bind(el);
     }
-    const sub = observer.subscribe({
-      next: nextResult,
-      error: catchError
-    });
 
     function nextResult({ data }) {
-      $apollo._changeLoader(loadingKey,false,loadingChangeCb);
-      $apollo._applyData(data,key);
+      $apollo._changeLoader(loadingKey, false, loadingChangeCb);
+      $apollo._applyData(data, key);
     }
+
     function catchError(error) {
-      $apollo._changeLoader(loadingKey,false,loadingChangeCb);
+      $apollo._changeLoader(loadingKey, false, loadingChangeCb);
       if (error.graphQLErrors && error.graphQLErrors.length !== 0) {
         console.error(`GraphQL execution errors for query ${key}`);
-        for (let e of error.graphQLErrors) {
+        for (const e of error.graphQLErrors) {
           console.error(e);
         }
       } else if (error.networkError) {
@@ -142,26 +94,31 @@ class DollarApollo {
         console.error(error);
       }
       if (typeof options.error === 'function') {
-        options.error.apply(el,[error]);
+        options.error.apply(el, [error]);
       }
     }
+
+    const sub = observer.subscribe({
+      next: nextResult,
+      error: catchError,
+    });
 
     return sub;
   }
 
-  _applyData(data,key) {
+  _applyData(data, key) {
     if (data[key] === undefined) {
       console.error(`Missing "${key}" in properties`, data);
     } else {
       const storeEntry = this.el.__apollo_store[key];
-      if(storeEntry && !storeEntry.firstLoadingDone){
+      if (storeEntry && !storeEntry.firstLoadingDone) {
         this.el.__apollo_store[key].firstLoadingDone = true;
       }
       this.el[key] = data[key];
     }
   }
 
-  _changeLoader(loadingKey,value,loadingChangeCb) {
+  _changeLoader(loadingKey, value, loadingChangeCb) {
     if (loadingKey) {
       this.el[loadingKey] = value;
     }
@@ -170,29 +127,26 @@ class DollarApollo {
       loadingChangeCb(value);
     }
   }
-  _refetch(key,options,variables,observer){
-    const  el = this.el;
+
+  _refetch(key, options, variables, observer) {
+    const el = this.el;
     const $apollo = this;
-    let loadingKey = options.loadingKey;
+    const loadingKey = options.loadingKey;
     let loadingChangeCb = options.watchLoading;
-    this._changeLoader(loadingKey,true,loadingChangeCb);
+    this._changeLoader(loadingKey, true, loadingChangeCb);
     if (typeof loadingChangeCb === 'function') {
       loadingChangeCb = loadingChangeCb.bind(el);
     }
-    observer.refetch(variables, {
-      forceFetch: !!options.forceFetch
-    }).then(nextResult,catchError);
-
     function nextResult({ data }) {
-      $apollo._changeLoader(loadingKey,false,loadingChangeCb);
-      $apollo._applyData(data,key);
+      $apollo._changeLoader(loadingKey, false, loadingChangeCb);
+      $apollo._applyData(data, key);
     }
     function catchError(error) {
-      $apollo._changeLoader(loadingKey,false,loadingChangeCb);
+      $apollo._changeLoader(loadingKey, false, loadingChangeCb);
 
       if (error.graphQLErrors && error.graphQLErrors.length !== 0) {
         console.error(`GraphQL execution errors for query ${key}`);
-        for (let e of error.graphQLErrors) {
+        for (const e of error.graphQLErrors) {
           console.error(e);
         }
       } else if (error.networkError) {
@@ -202,45 +156,51 @@ class DollarApollo {
       }
 
       if (typeof options.error === 'function') {
-        options.error.apply(el,[error]);
+        options.error.apply(el, [error]);
       }
     }
+
+    observer.refetch(variables, {
+      forceFetch: !!options.forceFetch,
+    }).then(nextResult, catchError);
   }
-  _processVariables(key,options,sub,observer){
-    let variables = options.variables;
+
+  _processVariables(key, options, sub, observer) {
+    const variables = options.variables;
     if (options.forceFetch && observer) {
       // Refresh query
-      this._refetch(key,options,variables,observer);
-    } else {
-      if (sub) {
-        sub.unsubscribe();
-      }
-
-      // Create observer
-      observer = this.watchQuery(this._generateApolloOptions(options));
-      this.el.__apollo_store[key] = {observer,variables,options};
-
+      this._refetch(key, options, variables, observer);
+      return observer;
     }
-    return observer;
+    if (sub) {
+      sub.unsubscribe();
+    }
+
+    // Create observer
+    const newObserver = this.watchQuery(this._generateApolloOptions(options));
+    this.el.__apollo_store[key] = { observer: newObserver, variables, options };
+    return newObserver;
   }
-  refetch(key){
+
+  refetch(key) {
     const obj = this.el.__apollo_store[key];
-    this._refetch(key,obj.options,obj.variables,obj.observer);
+    this._refetch(key, obj.options, obj.variables, obj.observer);
   }
+
   _generateApolloOptions(options) {
     const apolloOptions = omit(options, [
       'error',
       'loadingKey',
       'watchLoading',
-      'skip'
+      'skip',
     ]);
     return apolloOptions;
   }
 
   _addPolymerObserver(el, variable, observer) {
-    const rand = Math.floor(1000000000 + Math.random() * 9000000000);
-    const r_id = `__apollo_${rand}`;
-    el[r_id] = observer;
+    const rand = Math.floor(1000000000 + (Math.random() * 9000000000));
+    const rId = `__apollo_${rand}`;
+    el[rId] = observer;
     el.observers = el.observers || [];
     el.observers.push(`__apollo_${rand}(${variable})`);
   }
@@ -248,18 +208,14 @@ class DollarApollo {
   process(key, options) {
     const el = this.el;
     const $apollo = this;
-    let query, observer, sub;
-    // Simple query
-    if (!options.query) {
-      query = options;
-    }
+    let sub;
 
-    observer = this._processVariables(key,options,sub,observer);
+    const observer = this._processVariables(key, options, sub);
 
     if (options.skip !== undefined) {
-      let _var = options.skip;
+      const _var = options.skip;
 
-      this._addPolymerObserver(el, _var, function(newSkipValue){
+      this._addPolymerObserver(el, _var, (newSkipValue) => {
         const storeEntry = el.__apollo_store[key];
         if (!newSkipValue) {
           storeEntry.options.skip = false;
@@ -273,36 +229,74 @@ class DollarApollo {
         }
       });
 
-      const prop = deepFind(el.properties,_var);
-      if(prop !== undefined) {
+      const prop = deepFind(el.properties, _var);
+      if (prop !== undefined) {
         // assuming initial value is true if undefined
         options.skip = prop.value === undefined ? true : prop.value;
-      }
-      else {
+      } else {
         console.error(`Missing "${_var}" in properties, ignoring skip option`);
         delete options.skip;
       }
     }
 
-    for(let i in options.variables){
-      let _var = options.variables[i];
+    for (const i of Object.keys(options.variables)) {
+      if ({}.hasOwnProperty.call(options.variables, i)) {
+        const _var = options.variables[i];
 
-      this._addPolymerObserver(el, _var, function(newValue){
-        const storeEntry = el.__apollo_store[key];
-        if(storeEntry && storeEntry.firstLoadingDone){
-          options.variables[i] = newValue;
-          $apollo._refetch(key,options,options.variables,observer);
+        this._addPolymerObserver(el, _var, (newValue) => {
+          const storeEntry = el.__apollo_store[key];
+          if (storeEntry && storeEntry.firstLoadingDone) {
+            options.variables[i] = newValue;
+            $apollo._refetch(key, options, options.variables, observer);
+          }
+        });
+
+        const prop = deepFind(el.properties, _var);
+        if (prop !== undefined) {
+          options.variables[i] = prop.value;
+        } else {
+          console.error(`Missing "${i}" in properties`);
         }
-      });
-
-      const prop = deepFind(el.properties,_var);
-      if(prop !== undefined){
-        options.variables[i] = prop.value;
-      }
-      else{
-        console.error(`Missing "${i}" in properties`);
       }
     }
+  }
+}
 
+export class PolymerApollo {
+  constructor(options) {
+    this._apolloClient = options.apolloClient;
+    this._apolloSubscriptions = [];
+    this.properties = {};
+  }
+  beforeRegister() {
+    const apollo = this.apollo;
+    this.$apollo = new DollarApollo(this);
+
+    if (apollo) {
+      const queries = omit(apollo, [
+        'subscribe',
+      ]);
+
+      // watchQuery
+      for (const key of Object.keys(queries)) {
+        this.$apollo.process(key, queries[key]);
+      }
+      // subscribe
+      if (apollo.subscribe) {
+        // TODO
+      }
+    }
+  }
+  attached() {
+    this.$apollo.processObservers(this);
+  }
+  detached() {
+    this._apolloSubscriptions.forEach((sub) => {
+      sub.unsubscribe();
+    });
+    this._apolloSubscriptions = null;
+    if (this.$apollo) {
+      this.$apollo = null;
+    }
   }
 }
